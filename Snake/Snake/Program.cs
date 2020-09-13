42c6d64db73bac0aa338d3a5515dc55e62840854
Kirill Ryzhov <kirillruzhovmoscow1999@gmail.com>
2020-09-13 15:34:11 +0300

    Работа ОЗУ на примере программы.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,50 +8,59 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+
             Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
             Point p2 = new Point(4, 5, '#');
-            Point p3 = new Point(9, 4, '$');
-            Point p4 = new Point(7, 6, '@');
-
-            List<Point> plst = new List<Point>();
-
-            plst.Add(p1);
-            plst.Add(p2);
-            plst.Add(p3);
-            plst.Add(p4);
-
-            foreach(Point point in plst)
-            {
-                point.Draw();
-            }
-
+            p2.Draw();
 
             Console.WriteLine();
 
-            List<int> numlist = new List<int>();
+            int x = 1;
+            Func1(x);
+            Console.WriteLine($"Func1 x={x}");
 
-            numlist.Add(0);
-            numlist.Add(1);
-            numlist.Add(2);
+            Func2(x);
+            Console.WriteLine($"Func2 x={x}");
 
-            int x = numlist[0];
-            int y = numlist[1];
-            int z = numlist[2];
-           
-            foreach (int i in numlist)
-            {
-                Console.WriteLine(i);
-            }
+            Func3(x);
+            Console.WriteLine($"Func3 x={x}");
 
-            numlist.Remove(0);
+            Move(p1, 10, 10);
+            Console.WriteLine($"Move(p1, 10, 10) p1.x = {p1.x}, p1.y = {p1.y}");
 
-            List<Point> plist  = new List<Point>();
+            Reset(p2);
+            Console.WriteLine($"Reset(p2) p2.x = {p2.x}, p2.y = {p2.y}");
 
-
-            Console.ReadLine();
+            Console.ReadKey();
         }
 
-        
+        private static void Reset(Point p)
+        {
+            p = new Point();
+            //  Здесь переменная p2 останется без изменений.
+        }
+
+        private static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+
+        private static void Func3(int x)
+        {
+            x++;
+        }
+
+        private static void Func2(int val)
+        {
+            val++;
+        }
+
+        private static void Func1(int x)
+        {
+
+        }
     }
 }
-    
