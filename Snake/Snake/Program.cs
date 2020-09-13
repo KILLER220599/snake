db63259f59c647fe1aedfe127a481e3018e99b6f
Kirill Ryzhov <kirillruzhovmoscow1999@gmail.com>
2020-09-13 13:59:20 +0300

    Процедурный подход. Функция Draw.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Snake
 {
@@ -6,8 +7,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char symb1 = '*';
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(symb1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char symb2 = '#';
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(symb2);
+
             Console.ReadLine();
         }
     }
 }
+    
