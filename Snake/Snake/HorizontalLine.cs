c659445605d8afb664e5985b3ce6d1bbf19ed88b
Kirill Ryzhov <kirillruzhovmoscow1999@gmail.com>
2020-09-13 17:14:51 +0300

    Сделали горизонтальную линию.

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> plst;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symb)
+        {
+            plst = new List<Point>();
+
+            for(int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, symb);
+                plst.Add(p);
+            }
+
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in plst)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
